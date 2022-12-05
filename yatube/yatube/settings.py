24485bd294356a6deec9780cd938589b70db4395
Никита Бобков <n.bobkov99@bk.ru>
2022-12-05 23:26:53 +0300

    Финалk

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f%-$2v#hg6=cu6ls-mt+-4^an_nvbw4lk-zrqt!zvoij1@wsrg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
