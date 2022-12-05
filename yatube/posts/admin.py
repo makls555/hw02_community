b211218921e704edeaa66d532c35f52819e076ea
Никита Бобков <n.bobkov99@bk.ru>
2022-12-05 21:26:34 +0300

    Финал

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -3,11 +3,12 @@ from .models import Group, Post
 
 
 class PostAdmin(admin.ModelAdmin):
-     list_display = ('pk', 'text', 'pub_date', 'author', 'group')
-     search_fields = ('text',)
-     list_filter = ('pub_date',)
-     list_editable = ("group",)
-     empty_value_display = '-пусто-'
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    list_editable = ("group",)
+    empty_value_display = '-пусто-'
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
